1eee60f169b42df9a5ab3b16f2cf2d587b3e6c30
pranitv <52202377+pranitv@users.noreply.github.com>
2019-07-23 17:04:14 +0530

    Create Base.java

---
+++ Base.java
@@ -0,0 +1,53 @@
+package com.qa.hs.keyword.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Base {
+	
+	public WebDriver driver;
+	public Properties prop;
+	
+	public WebDriver init_driver(String browserName){
+		if(browserName.equals("chrome")){
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\VSPICT\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
+			if(prop.getProperty("headless").equals("yes")){
+				//headless mode:
+				ChromeOptions options = new ChromeOptions();
+				options.addArguments("--headless");
+				driver = new ChromeDriver(options);
+			}else{
+				driver = new ChromeDriver();
+			}
+		} else if(browserName.equals("firefox")){
+			System.setProperty("webdriver.gecko.driver", "/Users/NaveenKhunteta/Downloads/geckodriver");
+			driver = new FirefoxDriver();
+		}
+		return driver;
+	}
+	
+	public Properties init_properties(){
+		prop = new Properties();
+		try {
+			FileInputStream ip = new FileInputStream("C:\\Users\\VSPICT\\Downloads\\keyworddriven\\Keyword-Driven-Web-UI-Framework-master\\src\\main\\java\\com\\qa\\hs\\keyword\\config\\config.properties");
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return prop;
+	}
+	
+	
+	
+	
+
+}
